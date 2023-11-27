d5da1fcd7bbda6e1a2ca865bece018d47a6c245a
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2023-11-27 16:25:56 -0300

    :rocket: Project Finish

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,36 @@
 AOS.init();
+const dataDoEvento = new Date("Mar 14, 2024 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+const contaAsHoras = setInterval(function () {
+  const agora = new Date();
+  const timeStampAtual = agora.getTime();
+  const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+  const diasEmMs = 1000 * 60 * 60 * 24;
+  const horasEmMs = 1000 * 60 * 60;
+  const minutoEmMs = 1000 * 60;
+
+  const diasAteOEvento = Math.floor(distanciaAteOEvento / diasEmMs);
+  const horasAteOEvento = Math.floor(
+    (distanciaAteOEvento % diasEmMs) / horasEmMs
+  );
+  const minutosAteOEvento = Math.floor(
+    (distanciaAteOEvento % horasEmMs) / minutoEmMs
+  );
+  const segundosAteOEvento = Math.floor(
+    (distanciaAteOEvento % minutoEmMs) / 1000
+  );
+  console.log(diasAteOEvento);
+  console.log(horasAteOEvento);
+  console.log(minutosAteOEvento);
+  console.log(segundosAteOEvento);
+
+  document.getElementById(
+    "contador"
+  ).innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+
+  if (distanciaAteOEvento < 0) {
+    clearInterval(contaAsHoras);
+    document.getElementById("contador").innerHTML =
+      "Não acredito  Você perdeu ☹️ ";
+  }
+}, 1000);
